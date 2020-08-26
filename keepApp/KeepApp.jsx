2d12a97f88e746829046cda5455888bd61ff7937
ordamari <ordamari971@gmail.com>
2020-08-27 00:49:09 +0300

    add note option

--- keepApp/KeepApp.jsx
+++ keepApp/KeepApp.jsx
@@ -1,5 +1,6 @@
 import { keepService } from './services/keep-service.js'
 import { NotePreview } from './cmps/NotePreview.jsx'
+import { AddNote } from './cmps/AddNote.jsx'
 export class KeepApp extends React.Component {
 
     state = {
@@ -31,6 +32,11 @@ export class KeepApp extends React.Component {
         keepService.ChangeColor(noteId, color);
         this.loadNotes();
     }
+    
+    onAddNote = (type,info)=>{
+        keepService.create(type,info);
+        this.loadNotes();
+    }
 
 
 
@@ -38,6 +44,7 @@ export class KeepApp extends React.Component {
     render() {
         return (
             <section >
+                <AddNote onAddNote={this.onAddNote} />
                 {this.state.notes.map(note => <NotePreview key={note.id} note={note} onChangeColor={this.onChangeColor} onChangePinned={this.onChangePinned} onDelete={this.onDelete} />)}
             </section>
         )
---
+++ keepApp/cmps/AddNoteTodos.jsx
@@ -0,0 +1,51 @@
+import {TodoPreview} from './TodoPreview.jsx'
+export class AddNoteTodos extends React.Component {
+
+    state = {
+        todos: [],
+        newTodo: ''
+    }
+
+    handleChange = ({ target }) => {
+        this.setState({ newTodo: target.value }, () => { console.log(this.state.newTodo) })
+    }
+
+    handleSubmit = () => {
+        let todos = [...this.state.todos];
+        todos.push({ id: makeId(), txt:this.state.newTodo, doneAt: null});
+        this.setState({ todos, newTodo: '' }, () => { console.log(this.state.newTodo) });
+    }
+
+    removeTodo = (todoId)=>{
+        this.setState({todos:this.state.todos.filter(todo=>todo.id!==todoId)})
+    }
+
+    onSaveTodo = ()=>{
+        this.props.addTodo(this.state.todos)
+    } 
+
+
+
+    render() {
+        return (
+            <div>
+                <div className='todo-list'>
+                {this.state.todos.map((todo,idx) => <TodoPreview removeTodo={this.removeTodo} key={todo.id} todo={todo}/>)}
+                </div>
+
+                <div className='add-note-todos'>
+                    <form onSubmit={this.handleSubmit}>
+                        <input value={this.state.newTodo} type="text" placeholder="Add todo"
+                            onChange={this.handleChange} />
+                        <button onClick={() => this.handleSubmit}><i className="fas fa-plus"></i></button>
+                        <button onClick={() => this.onSaveTodo()}><i className="far fa-save"></i></button>
+                    </form>
+
+
+
+                </div>
+            </div>
+        )
+    }
+}
+
