b1d9a82c7009cf4e362b1e1f938fb1590bb953da
roi <bendaroi@gmail.com>
2020-08-28 10:46:31 +0300

    sadsa

--- cmps/Search.jsx
+++ cmps/Search.jsx
@@ -1,11 +1,15 @@
 const { NavLink,withRouter } = ReactRouterDOM
 class _Search extends React.Component {
     state={
+        page:'/'
 
     }
 
     componentDidMount() {
         console.log(this.props);
+    }
+    componentDidUpdate() {
+        console.log(this.props.location.pathname);
     }
  
     render() {
