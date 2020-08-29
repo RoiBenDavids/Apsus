17f08f78e09209c4ef2e2b9ee8713432684990c4
roi <bendaroi@gmail.com>
2020-08-29 12:21:23 +0300

    sadas

--- mailApp/cmps/MailCompose.jsx
+++ mailApp/cmps/MailCompose.jsx
@@ -7,7 +7,6 @@ export class MailCompose extends React.Component {
     }
 
     componentDidMount() { 
-       console.log( this.props.noteToCompose);
         if (this.props.noteToCompose.body) this.setState({subject:'From my notes '+this.props.noteToCompose.subject,body:this.props.noteToCompose.body})
      
      
@@ -25,7 +24,7 @@ export class MailCompose extends React.Component {
                 <input type='text' name='to' placeholder='to' value={ this.state.to } onChange={this.onInputChange}/>
                 <input type='text' name='subject' placeholder='subject' value={ this.state.subject } onChange={this.onInputChange}/>
                 <textarea name='body' text={ this.state.body } onChange={this.onInputChange} value={ this.state.body }/>
-               <div className='compose-footer flex align-center'><button>send</button></div>
+               <div className='compose-footer flex align-center'><button><i className=" fas fa-paper-plane"></i></button></div>
                 
 
             </form>
